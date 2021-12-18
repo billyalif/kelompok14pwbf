a5bf378d9f038fb3a89b5d8b1242127f51d080cd
tommyrachmadiono <tommyrachmadiono@gmail.com>
2021-12-18 12:30:56 +0700

    update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,23 @@
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Viola Boutique</title>
-        <link href="css/login.css" rel="stylesheet">
-    </head>
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viola Boutique</title>
+    <link href="css/login.css" rel="stylesheet">
+</head>
+
+<body>
     <div class="container" id="container">
-        <div class="form-container sign-up-container">
-            <form action="#">
-            </form>
-        </div>
         <div class="form-container sign-in-container">
-            <form action="/pegawai" method="get">
+            <form action="{{ route('login') }}" method="GET">
                 <h1>Pegawai</h1>
-                <p ></p>
+                <p></p>
                 <button class="button" id="signUp">
-                <a href="/pegawai" >Login</a>
+                    Login
+                </button>
             </form>
         </div>
         <div class="overlay-container">
@@ -27,19 +26,18 @@
                     <h1>Owner</h1>
                     <p> </p>
                     <button class="button" id="signUp">
-                    <a href="/owner" >Login</a>
+                        <a href="{{ route('login') }}">Login</a>
                     </button>
 
                     <p> </p>
                     <button class="button" id="signUp">
-                    <a href="/register" >Register</a>
+                        <a href="{{ route('register.index') }}">Register</a>
                     </button>
 
                 </div>
             </div>
         </div>
     </div>
+</body>
 
-
-    </body>
-    </html>
+</html>
