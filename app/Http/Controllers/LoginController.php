b0005384610c8608b5a50486e8fb8b07b9265ec6
tommyrachmadiono <tommyrachmadiono@gmail.com>
2021-12-18 12:28:35 +0700

    fix controller for auth

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,28 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
-    public function index(){
-        return view('login.index',[
-            'title'=>'Login'
-        ]);
+    public function index()
+    {
+        return view('login');
     }
+
     public function authenticate(Request $request)
     {
-        $credentials = $request -> validate([
+        $credentials = $request->validate([
             'email'   => 'required|email:dns',
-            'password'=> 'required',
+            'password' => 'required',
         ]);
 
-        if(Auth::attempt($credentials)){
-            $request->session()->regenerate();
-
-            return redirect()->intended('/dashboard');
+        if (Auth::attempt($credentials)) {
+            if (Auth::user()->role->jenis_role == 'Pegawai') {
+                return redirect()->intended('/pegawai');
+            }
+            return redirect()->intended('/owner');
         };
 
         return back()->with('loginError', 'Login Gagal!');
     }
-}
+
+    public function logout()
+    {
+        Auth::logout();
+        Session::flush();
+        return redirect('/');
+    }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kota;
+use App\Models\Role;
+use App\Models\TabelUser;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function index(){
-        return view('pemilik.register');
+    public function index()
+    {
+        $data['roles'] = Role::all();
+        $data['cities'] = Kota::all();
+
+        return view('pemilik.register', $data);
     }
-    public function store(Request $request){
+
+    public function store(Request $request)
+    {
         $validatedData = $request->validate([
             'nama'          => 'required|min:3|max:50',
             'alamat'        => 'required|max:75',
@@ -18,13 +27,13 @@ class RegisterController extends Controller
             'email'         => 'required|email:dns|unique:tabel_user',
             'telp'          => 'required|max:20',
             'username'      => 'required|min:3|max:50|unique:tabel_user',
-            'password'      => 'required||min:8|max:32'
+            'password'      => 'required|min:8|max:32'
         ]);
-    $validatedData['password'] = bcrypt($validatedData['password']);
-    TabelUserController::create($validatedData);
+        $validatedData['password'] = bcrypt($validatedData['password']);
+        TabelUser::create($validatedData);
 
-    $request->session()->flash('success','Registrasi Berhasil! Silahkan Login');
+        $request->session()->flash('success', 'Registrasi Berhasil! Silahkan Login');
 
-    return redirect('/login');
+        return redirect()->route('login');
     }
 };
